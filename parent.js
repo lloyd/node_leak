9c8a6c43d8d459f457849926c225b1add3003dc2
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-01-09 14:39:11 -0700

    log more information to console on forwarding errors, and output heap usage on SIGHUP

--- parent.js
+++ parent.js
@@ -10,9 +10,13 @@ var kid = child_process.fork('./child.js');
 kid.on('message', function(m) {
   var server = http.createServer(function (req, res) {
     forward('http://127.0.0.1:' + m, req, res, function(err) {
-      if (err) console.log('error!');
+      if (err) console.log('error', err);
     });
   }).listen(0, "127.0.0.1", function() {
     console.log('forwarder running on http://127.0.0.1:' + server.address().port);
   });
+});
+
+process.on('SIGHUP', function() {
+  console.log(process.memoryUsage());
 });
